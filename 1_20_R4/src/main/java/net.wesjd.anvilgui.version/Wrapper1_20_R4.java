f07a75226e92dfe4ea80d16a751ab915e94a0b44
mastercake10 <dev@spaceio.de>
2024-05-12 16:51:22 +0200

    add support for mc 1.20.5/6
    closes #323

---
+++ 1_20_R4/src/main/java/net.wesjd.anvilgui.version/Wrapper1_20_R4.java
@@ -0,0 +1,146 @@
+package net.wesjd.anvilgui.version;
+
+import net.minecraft.core.BlockPosition;
+import net.minecraft.core.IRegistryCustom;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.PacketPlayOutCloseWindow;
+import net.minecraft.network.protocol.game.PacketPlayOutOpenWindow;
+import net.minecraft.server.level.EntityPlayer;
+import net.minecraft.world.IInventory;
+import net.minecraft.world.entity.player.EntityHuman;
+import net.minecraft.world.inventory.*;
+import org.bukkit.craftbukkit.v1_20_R4.CraftWorld;
+import org.bukkit.craftbukkit.v1_20_R4.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_20_R4.event.CraftEventFactory;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public final class Wrapper1_20_R4 implements VersionWrapper {
+    private int getRealNextContainerId(Player player) {
+        return toNMS(player).nextContainerCounter();
+    }
+
+    /**
+     * Turns a {@link Player} into an NMS one
+     *
+     * @param player The player to be converted
+     * @return the NMS EntityPlayer
+     */
+    private EntityPlayer toNMS(Player player) {
+        return ((CraftPlayer) player).getHandle();
+    }
+
+    @Override
+    public int getNextContainerId(Player player, AnvilContainerWrapper container) {
+        return ((AnvilContainer) container).getContainerId();
+    }
+
+    @Override
+    public void handleInventoryCloseEvent(Player player) {
+        CraftEventFactory.handleInventoryCloseEvent(toNMS(player));
+        toNMS(player).s(); // s -> doCloseContainer
+    }
+
+    @Override
+    public void sendPacketOpenWindow(Player player, int containerId, Object inventoryTitle) {
+        toNMS(player).c.b(new PacketPlayOutOpenWindow(containerId, Containers.i, (IChatBaseComponent) inventoryTitle));
+    }
+
+    @Override
+    public void sendPacketCloseWindow(Player player, int containerId) {
+        toNMS(player).c.b(new PacketPlayOutCloseWindow(containerId));
+    }
+
+    @Override
+    public void setActiveContainerDefault(Player player) {
+        toNMS(player).cb = toNMS(player).ca; // cb -> containerMenu, ca -> inventoryMenu
+    }
+
+    @Override
+    public void setActiveContainer(Player player, AnvilContainerWrapper container) {
+        toNMS(player).cb = (Container) container;
+    }
+
+    @Override
+    public void setActiveContainerId(AnvilContainerWrapper container, int containerId) {}
+
+    @Override
+    public void addActiveContainerSlotListener(AnvilContainerWrapper container, Player player) {
+        toNMS(player).a((Container) container);
+    }
+
+    @Override
+    public AnvilContainerWrapper newContainerAnvil(Player player, Object title) {
+        return new AnvilContainer(player, getRealNextContainerId(player), (IChatBaseComponent) title);
+    }
+
+    @Override
+    public Object literalChatComponent(String content) {
+        return IChatBaseComponent.b(content); // IChatBaseComponent.b -> Component.literal
+    }
+
+    @Override
+    public Object jsonChatComponent(String json) {
+        return IChatBaseComponent.ChatSerializer.a(json, IRegistryCustom.b);
+    }
+
+    private static class AnvilContainer extends ContainerAnvil implements AnvilContainerWrapper {
+        public AnvilContainer(Player player, int containerId, IChatBaseComponent guiTitle) {
+            super(
+                    containerId,
+                    ((CraftPlayer) player).getHandle().gc(),
+                    ContainerAccess.a(((CraftWorld) player.getWorld()).getHandle(), new BlockPosition(0, 0, 0)));
+            this.checkReachable = false;
+            setTitle(guiTitle);
+        }
+
+        @Override
+        public void m() {
+            // If the output is empty copy the left input into the output
+            Slot output = this.b(2); // b -> getSlot
+            if (!output.h()) { // h -> hasItem
+                output.f(this.b(0).g().s()); // f -> set, g -> getItem, s -> copy
+            }
+
+            this.w.a(0); // w -> cost, a -> set
+
+            // Sync to the client
+            this.b(); // b -> sendAllDataToRemote
+            this.d(); // d -> broadcastChanges
+        }
+
+        @Override
+        public void b(EntityHuman player) {}
+
+        @Override
+        protected void a(EntityHuman player, IInventory container) {}
+
+        public int getContainerId() {
+            return this.j;
+        }
+
+        @Override
+        public String getRenameText() {
+            return this.v;
+        }
+
+        @Override
+        public void setRenameText(String text) {
+            // If an item is present in the left input slot change its hover name to the literal text.
+            Slot inputLeft = b(0);
+            if (inputLeft.h()) {
+                inputLeft
+                        .g()
+                        .b(
+                                DataComponents.g,
+                                IChatBaseComponent.b(text)); // DataComponents.g -> DataComponents.CUSTOM_NAME
+            }
+        }
+
+        @Override
+        public Inventory getBukkitInventory() {
+            return getBukkitView().getTopInventory();
+        }
+    }
+}
--- api/src/main/java/net/wesjd/anvilgui/version/VersionMatcher.java
+++ api/src/main/java/net/wesjd/anvilgui/version/VersionMatcher.java
@@ -1,6 +1,5 @@
 package net.wesjd.anvilgui.version;
 
-
 import org.bukkit.Bukkit;
 
 /**
@@ -18,22 +17,36 @@ public class VersionMatcher {
      * @throws IllegalStateException If the version wrapper failed to be instantiated or is unable to be found
      */
     public VersionWrapper match() {
-        final String serverVersion = Bukkit.getServer()
-                .getClass()
-                .getPackage()
-                .getName()
-                .split("\\.")[3]
-                .substring(1);
+
+        String craftBukkitPackage = Bukkit.getServer().getClass().getPackage().getName();
+
+        String rVersion;
+        if (!craftBukkitPackage.contains(".v")) { // cb package not relocated (i.e. paper 1.20.5+)
+            // separating major and minor versions, example: 1.20.4-R0.1-SNAPSHOT -> major = 20, minor = 4
+            final String[] versionNumbers =
+                    Bukkit.getBukkitVersion().split("-")[0].split("\\.");
+            int major = Integer.parseInt(versionNumbers[1]);
+            int minor = Integer.parseInt(versionNumbers[2]);
+
+            if (major == 20 && minor >= 5) {
+                rVersion = "1_20_R4";
+            } else {
+                throw new IllegalStateException(
+                        "AnvilGUI does not support bukkit server version \"" + Bukkit.getBukkitVersion() + "\"");
+            }
+
+        } else {
+            rVersion = craftBukkitPackage.split("\\.")[3].substring(1);
+        }
+
         try {
-            return (VersionWrapper) Class.forName(getClass().getPackage().getName() + ".Wrapper" + serverVersion)
+            return (VersionWrapper) Class.forName(getClass().getPackage().getName() + ".Wrapper" + rVersion)
                     .getDeclaredConstructor()
                     .newInstance();
         } catch (ClassNotFoundException exception) {
-            throw new IllegalStateException(
-                    "AnvilGUI does not support server version \"" + serverVersion + "\"", exception);
+            throw new IllegalStateException("AnvilGUI does not support server version \"" + rVersion + "\"", exception);
         } catch (ReflectiveOperationException exception) {
-            throw new IllegalStateException(
-                    "Failed to instantiate version wrapper for version " + serverVersion, exception);
+            throw new IllegalStateException("Failed to instantiate version wrapper for version " + rVersion, exception);
         }
     }
 }
